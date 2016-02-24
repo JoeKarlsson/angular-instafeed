5782e3aab71c321743546a8d18fd23333ba3d4c1
Joe Karlsson <joe.carlson@callmejoe.net>
2016-02-23 16:58:25 -1000

    Cleaned up gulpfiles to just the package

---
+++ public/js/instafeed.js
@@ -0,0 +1,105 @@
+'use strict'
+
+let createPhotoGrid = () => {
+  let instafeed = document.getElementById('instafeed');
+
+  let ngApp = document.createElement('div');
+  ngApp.setAttribute('ng-app', 'instafeed');
+  instafeed.appendChild(ngApp);
+
+  let ngControllerSection = document.createElement('section');
+  ngControllerSection.setAttribute('ng-controller', 'ShowImages as images');
+  ngApp.appendChild(ngControllerSection);
+
+  let bar = document.createElement('div');
+  bar.className = 'bar';
+  ngControllerSection.appendChild(bar);
+
+  let listIcon = document.createElement('a');
+  listIcon.className = 'list-icon';
+  bar.appendChild(listIcon);
+  listIcon.setAttribute('ng-class', '{active: layout == \'list\'}');
+  listIcon.setAttribute('ng-click', 'layout = \'list\'');
+
+  let gridIcon = document.createElement('a');
+  gridIcon.className = 'grid-icon';
+  bar.appendChild(gridIcon);
+  gridIcon.setAttribute('ng-class', '{active: layout == \'grid\'}');
+  gridIcon.setAttribute('ng-click', 'layout = \'grid\'');
+
+  // A view with big photos and no text
+  let gridList = document.createElement('ul');
+  gridList.className = 'grid';
+  gridList.setAttribute('ng-show', 'layout == \'grid\'');
+  ngControllerSection.appendChild(gridList);
+
+  let photoGridItem = document.createElement('li');
+  photoGridItem.className = 'grid';
+  photoGridItem.setAttribute('ng-repeat', 'p in pics');
+  gridList.appendChild(photoGridItem);
+
+  let photoLinkGrid = document.createElement('a');
+  photoLinkGrid.href = '{{p.link}}';
+  photoLinkGrid.target = '_blank';
+  photoGridItem.appendChild(photoLinkGrid);
+
+  let photoGridImg = document.createElement('img');
+  photoGridImg.setAttribute('ng-src', '{{p.images.low_resolution.url}}');
+  photoLinkGrid.appendChild(photoGridImg);
+
+  // A compact view smaller photos and titles
+  let listList = document.createElement('ul');
+  listList.className = 'list';
+  listList.setAttribute('ng-show', 'layout == \'list\'');
+  ngControllerSection.appendChild(listList);
+
+  let photoListItem = document.createElement('li');
+  photoListItem.className = 'grid';
+  photoListItem.setAttribute('ng-repeat', 'p in pics');
+  listList.appendChild(photoListItem);
+
+  let photoListLink = document.createElement('a');
+  photoListLink.href = '{{p.link}}';
+  photoListLink.target = '_blank';
+  photoListItem.appendChild(photoListLink);
+
+  let photoListImg = document.createElement('img');
+  photoListImg.setAttribute('ng-src', '{{p.images.low_resolution.url}}');
+  photoListItem.appendChild(photoListImg);
+
+  let caption = document.createElement('p');
+  caption.innerHTML = '{{p.caption.text}}';
+  photoListItem.appendChild(caption);
+
+}
+
+module.exports = function(client_id, user_id) {
+
+  createPhotoGrid();
+
+  let app = angular.module('instafeed', []);
+
+  app.factory('InstagramAPI', ['$http', function($http) {
+    return {
+      fetchPhotos : function(callback) {
+        let endpoint = 'https://api.instagram.com/v1/users/' + user_id + '/media/recent/?';
+        endpoint += '?count=99';
+        endpoint += '&client_id=' + client_id;
+        endpoint += '&callback=JSON_CALLBACK';
+        $http.jsonp(endpoint).success(function(response) {
+          callback(response.data);
+        });
+      }
+    }
+  }]);
+
+  app.controller('ShowImages', function($scope, InstagramAPI) {
+    $scope.layout = 'grid';
+    $scope.data = {};
+    $scope.pics = [];
+
+    InstagramAPI.fetchPhotos(function(data) {
+      $scope.pics = data;
+    });
+  });
+}
