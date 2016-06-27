67fedc287a7ad17d22434a9a4befcf31f210d340
Joe Karlsson <joe.carlson@callmejoe.net>
2016-06-26 15:31:33 -1000

    Updated API to connect with the updated Instagram auth flow.

--- public/js/instafeed.js
+++ public/js/instafeed.js
@@ -1,22 +1,31 @@
 (function() {
   //Place your own Instagram client_id below. Go to https://instagram.com/developer/clients/manage/ and register your app to get a client ID
-  var client_id = '[yourClientID]';
+   var client_id = '64a12cb0db7b41da8cd2a8736770c466';
 
   //To get your user ID go to http://jelled.com/instagram/lookup-user-id and enter your Instagram user name to get your user ID
-  var user_id = '[yourUserID]';
+  var user_id = '464793431';
+
+  //https://www.instagram.com/oauth/authorize/?client_id=64a12cb0db7b41da8cd2a8736770c466&redirect_uri=http://127.0.0.1:8080/test&response_type=token
+   var access_token = '464793431.64a12cb.9e7f0e0fd8ba4361be3205810e02cb73';
 
   var app = angular.module('instafeed', []);
 
   app.factory('InstagramAPI', ['$http', function($http) {
     return {
       fetchPhotos : function(callback) {
-        var endpoint = 'https://api.instagram.com/v1/users/' + user_id + '/media/recent/?';
+        var endpoint = 'https://api.instagram.com/v1/users/';
+        endpoint += user_id;
+        endpoint += '/media/recent/?';
         endpoint += '?count=99';
-        endpoint += '&client_id=' + client_id;
         endpoint += '&callback=JSON_CALLBACK';
-        $http.jsonp(endpoint).success(function(response) {
+        endpoint += '&access_token=' + access_token;
+        $http.jsonp(endpoint)
+        .success(function(response) {
           callback(response.data);
-        });
+        })
+        .error(function(xhr, status, err) {
+          console.error(status, err);
+        })
       }
     }
   }]);
